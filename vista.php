31b9709b56b7bef56b410a054f55a4de0aab4ba1
FRANCISCO SANTOS GUERRA <francisco.sangue.1@educa.jcyl.es>
2025-11-27 11:40:16 +0100

    Vista casi hecha

--- vista.php
+++ vista.php
@@ -6,6 +6,30 @@
     <title>Vista Inscripcion</title>
 </head>
 <body>
-    
+    <h1>Gestor de Inscripciones (File-to-DB)</h1>
+    <br>
+    <?php
+        echo $mensaje;
+    ?>
+    <br>
+    <form method="post">
+        <h2>Acciones del Torneo</h2>
+        <input type="submit" value="Volcar a Base de Datos">
+        <input type="submit" value="Reiniciar Fichero">
+        <br>
+        <h2>Inscribir Jugador</h2>
+        <label for="nick">Nickname:</label>
+        <input type="text" name="nick" id="nick" placeholder="Ej: Faker">
+        <label for="nivel">Nivel:</label>
+        <select name="nivel" id="nivel">
+            <option value="principiante" selected>Principiante (Hierro - Oro)</option>
+            <option value="intermedio">Intermedio (Platino - Diamante)</option>
+            <option value="experto">Experto (Master - Challenger)</option>
+        </select>
+        <label for="equipo">Nombre del Equipo (Opcional)</label>
+        <input type="text" name="equipo" id="equipo" placeholder="Ej: T1">
+        <br>
+        <input type="submit" value="INSCRIBIRSE AL FICHERO">
+    </form>
 </body>
 </html>
